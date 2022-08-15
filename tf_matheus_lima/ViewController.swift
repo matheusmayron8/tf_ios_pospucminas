289645dd70f1fd651ae1fe892dbf3202f9c245a8
marcelookasaki <marcelo.okasaki@gmai.com>
2022-08-15 19:08:16 -0400

    Initial commit. Project created.

---
+++ tf_matheus_lima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tf_matheus_lima
+//
+//  Created by user219122 on 15/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tf_matheus_limaUITests/tf_matheus_limaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tf_matheus_limaUITestsLaunchTests.swift
+//  tf_matheus_limaUITests
+//
+//  Created by user219122 on 15/08/22.
+//
+
+import XCTest
+
+class tf_matheus_limaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
